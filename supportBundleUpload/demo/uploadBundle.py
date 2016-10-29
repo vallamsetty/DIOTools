7e9e453379b8c049ce2b4de4006f320bf975deb7
Uday Vallamsetty <vallamsetty@gmail.com>
2016-10-28 19:00:37 -0700

    upload script creates the directory stucture and uploads a file.

--- supportBundleUpload/demo/uploadBundle.py
+++ supportBundleUpload/demo/uploadBundle.py
@@ -6,7 +6,14 @@ from boxsdk import Client
 from boxsdk.exception import BoxAPIException
 from boxsdk.object.collaboration import CollaborationRole
 from auth import authenticate
-
+from boxsdk import JWTAuth
+import shelve
+import argparse
+import sys
+import ConfigParser
+import datetime
+import time
+from pprint import pprint
 
 def run_user_example(client):
     # 'me' is a handy value to get info on the current authenticated user.
@@ -17,62 +24,11 @@ def run_user_example(client):
 def run_folder_examples(client):
     root_folder = client.folder(folder_id='0').get()
     print('The root folder is owned by: {0}'.format(root_folder.owned_by['login']))
-
     items = root_folder.get_items(limit=100, offset=0)
     print('This is the first 100 items in the root folder:')
     for item in items:
         print("   " + item.name)
 
-def find_folder_examples(client):
-    root_folder = client.folder(folder_id='0').get()
-    print('The root folder {0} is owned by: {1}'.format(root_folder.name, root_folder.owned_by['login']))
-
-    try:
-        items = root_folder.get_items(limit=100, offset=0)
-    except BoxAPIException:
-        print('get_items failed')
-    print ('finished get items, number of items = {0}'.format(len(items)))
-    testVar = raw_input("Please provide name of file to be uploaded:")
-    print('{0} uploaded: '.format(testVar))
-    file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), testVar)
-    try:
-        a_file = root_folder.upload(file_path, file_name=testVar)
-    except BoxAPIException:
-        print('upload failed')
-        pass
-
-    for item in items:
-        print ("im here")
-        print('Directory entry:{0}'.format(item.name))
-        if (item.name == 'SupportBundle'):
-            root_folder = client.folder(item.id)
-            testVar = raw_input("Please provide name of file to be uploaded:")
-            print('{0} uploaded: '.format(testVar))
-            file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), testVar)
-            a_file = root_folder.upload(file_path, file_name=testVar)
-            try:
-                print('{0} uploaded: '.format(a_file.get()['name']))
-            except BoxAPIException:
-                print('{0} upload failed: '.format(a_file.get()['name']))
-                print("   " + item.name)
-
-def run_collab_examples(client):
-    root_folder = client.folder(folder_id='0')
-    collab_folder = root_folder.create_subfolder('TestSupportBundle1')
-    try:
-        print('Folder {0} created'.format(collab_folder.get()['name']))
-        collaboration = collab_folder.add_collaborator('shalabh.goyal@datos.io', CollaborationRole.VIEWER)
-        print('Created a collaboration')
-        try:
-            modified_collaboration = collaboration.update_info(role=CollaborationRole.EDITOR)
-            print('Modified a collaboration: {0}'.format(modified_collaboration.role))
-        finally:
-#            collaboration.delete()
-            print('Deleted a collaboration')
-    finally:
-        # Clean up
-#        print('Delete folder collab folder succeeded: {0}'.format(collab_folder.delete()))
-        print("finally here")
 
 
 def rename_folder(client):
@@ -92,7 +48,6 @@ def get_folder_shared_link(client):
     collab_folder = root_folder.create_subfolder('shared link folder')
     try:
         print('Folder {0} created'.format(collab_folder.get().name))
-
         shared_link = collab_folder.get_shared_link()
         print('Got shared link:' + shared_link)
     finally:
@@ -303,11 +258,6 @@ def run_metadata_example(client):
     finally:
         foo.delete()
 
-from boxsdk import JWTAuth
-
-import shelve
-
-
 def store_tokens_callback_method(access_token, refresh_token):
     # store the tokens at secure storage (e.g. Keychain)
     d =  shelve.open("db.shlv")
@@ -316,7 +266,7 @@ def store_tokens_callback_method(access_token, refresh_token):
     d.close()
     return
 
-def run_examples_auth():
+def get_auth():
         
     auth = JWTAuth(
         client_id='37vdfnknzax5htrkiler5xkphbxs6f4s',
@@ -325,43 +275,171 @@ def run_examples_auth():
         jwt_key_id='h4qpyf9b',
         rsa_private_key_file_sys_path='private_key.pem',
 	rsa_private_key_passphrase=b'datos1234',
- #       store_tokens=store_tokens_callback_method,
     )
 
     access_token = auth.authenticate_instance()
-
-#    auth._user_id = None
-#    access_token = auth._auth_with_jwt(auth._enterprise_id, 'Business')
-
     client = Client(auth)
 
+    print("Upload authenticated successfully")
+
+    return client
+
+def handle_cli():
+
+    parser = argparse.ArgumentParser(description="Document upload to Box through API")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("-s", "--setup", action="count", help="Setup the Initial Configuration File (One time operation)")
+    group.add_argument("-l", "--logs", action="count", help="Upload Support Bundle")
+    group.add_argument("-c", "--callhome", action="count", help="Send Call Home Report")
+    args = parser.parse_args()    
+    if len(sys.argv)==1:
+        parser.print_help()
+        sys.exit(1)
+    return args
+
+def gen_config_file():
+
+    # Sample config file
+    # [General]
+    # Customer-Name = "Home Depot"
+    # Datos-Cluster-Name = "HD_Staging"
+    # Datos-Cluster-Id = 0
+    # Box-User-Login = "venkat.reddimanchu@box.com"
+    
+    # lets create that config file for next time...
+    cfgfile = open("box.cfg",'w')
+    
+    # add the settings to the structure of the file, and lets write it out...
+    Config = ConfigParser.RawConfigParser()
+    Config.add_section('General')
+    Config.set('General', '#Please do not edit the values below.')
+    cust_name = raw_input("Please provide customer name:")
+    Config.set('General', 'Customer-Name',cust_name)
+    datos_name = raw_input("Please provide identifying name for this Datos cluster:")
+    Config.set('General', 'Datos-Cluster-Name',datos_name)
+    datos_id = raw_input("Please provide identifying id (number) for this Datos cluster (This will be used for further communication w/ Datos):")
+    Config.set('General', 'Datos-Cluster-Id',datos_id)
+    box_id = raw_input("Please provide login (typically email address) for the user with privileges to the Datos Box folder:")
+    Config.set('General', 'Box-User-Login',box_id)
+    Config.write(cfgfile)
+    cfgfile.close()
+    print("Configuration File Generated Successfully")
+
+def get_folder_id(client, folder_name, folder_id=0):
+    # get the folder of the folder under folder with id = folder_id
+    root_folder = client.folder(folder_id).get()
     try:
-        users = client.users()
+        items = root_folder.get_items(limit=100, offset=0)
     except BoxAPIException:
-        print('users failed')
+        print('get_items failed')
+    for item in items:
+#        pprint(vars(item))
+#        print('Entry: Name = {0}, ID = {1}, Owned by {2}'.format(item.name, item.id, item.owned_by['login']))
+        if (item.name == folder_name):
+ #           root_folder = client.folder(item.id)
+ #           pprint(vars(root_folder))
+ #           print('The root folder {0} is owned by: {1}'.format(root_folder.name, root_folder.owned_by['login']))
+            return item.id
 
-    for user in users:
-        print("im here")
-        print('Username = {0}, Login = {1}'.format(user.name,user.login))
+def upload_logs(client):
 
-    run_user_example(client)
+    # Handle case when the folder is empty or the customers folder is not present
+    # Handle case with incomplete config file or if config file is not present
+    
+    config = ConfigParser.RawConfigParser()
+    config.read('box.cfg')
+    
+    datos_customer_name = config.get('General', 'Customer-Name')
+    datos_cluster_name = config.get('General', 'Datos-Cluster-Name')
+    
+    # Create a "SupportBundles_API" folder under root folder
+    root_folder_id = 0 #default folder id for root folder
+    support_bundle_folder_id = get_folder_id(client, "SupportBundles_API", root_folder_id)
+    root_folder = client.folder(support_bundle_folder_id)
+    try:
+        root_folder.add_collaborator('shalabh.goyal@datos.io', CollaborationRole.EDITOR)
+    except:
+        pass
+    if (support_bundle_folder_id is None):
+        root_folder = client.folder(root_folder_id)
+        support_bundle_folder = root_folder.create_subfolder("SupportBundles_API")
+        support_bundle_folder_id = support_bundle_folder.id
 
-    find_folder_examples(client)
+    print ("Support bundle folder id = {0}".format(support_bundle_folder_id))
+    # Create a "HomeDepot" folder under "SupportBundles_API" folder
+    cust_folder_id = get_folder_id(client, datos_customer_name, support_bundle_folder_id)
+    if (cust_folder_id is None):
+        root_folder = client.folder(support_bundle_folder_id)
+        cust_folder = root_folder.create_subfolder(datos_customer_name)
+        cust_folder_id = cust_folder.id
 
-#    run_collab_examples(client)
+    # Create a "Staging" folder under "HomeDepot" folder
+    datos_cluster_folder_id = get_folder_id(client, datos_cluster_name,cust_folder_id)
+    if (datos_cluster_folder_id is None):
+        root_folder = client.folder(cust_folder_id)
+        datos_cluster_folder = root_folder.create_subfolder(datos_cluster_name)
+        datos_cluster_folder_id = datos_cluster_folder.id
 
+    # Create a "Timestamp" folder under "Staging" folder
+    timestamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y_%m_%d_%H') + "_Hrs"
+    datos_timestamp_folder_id = get_folder_id(client, timestamp, datos_cluster_folder_id)
+    if (datos_timestamp_folder_id is None):
+        root_folder = client.folder(datos_cluster_folder_id)
+        datos_timestamp_folder = root_folder.create_subfolder(timestamp)
+        datos_timestamp_folder_id = datos_timestamp_folder.id
 
-#     # Premium Apps only
-#     upload_accelerator(client)
+    testVar = raw_input("Please provide name of file to be uploaded:")
+    file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), testVar)
+    root_folder = client.folder(datos_timestamp_folder_id)
+    a_file = root_folder.upload(file_path, file_name=testVar, upload_using_accelerator=True)
+    try:
+        print('{0} uploaded: '.format(a_file.get()['name']))
+    except BoxAPIException:
+        print('{0} upload failed: '.format(a_file.get()['name']))
+        print("   " + item.name)
+    print('{0} uploaded: '.format(testVar))
 
+def run_collab_examples(client):
+    root_folder = client.folder(folder_id='0')
+    collab_folder = root_folder.create_subfolder('TestSupportBundle1')
+    try:
+        print('Folder {0} created'.format(collab_folder.get()['name']))
+        collaboration = collab_folder.add_collaborator('shalabh.goyal@datos.io', CollaborationRole.VIEWER)
+        print('Created a collaboration')
+        try:
+            modified_collaboration = collaboration.update_info(role=CollaborationRole.EDITOR)
+            print('Modified a collaboration: {0}'.format(modified_collaboration.role))
+        finally:
+#            collaboration.delete()
+            print('Deleted a collaboration')
+    finally:
+        # Clean up
+#        print('Delete folder collab folder succeeded: {0}'.format(collab_folder.delete()))
+        print("finally here")
 
 
 def main():
 
-    # Please notice that you need to put in your client id and client secret in demo/auth.py in order to make this work.
-#    oauth, _, _ = authenticate()
-    run_examples_auth()
+    args = handle_cli()
+
+    if (args.setup):
+        gen_config_file()
+    elif (args.logs):
+        upload_logs(get_auth())
+    elif (args.callhome):
+        send_callhome(get_auth())
+
     os._exit(0)
+        
+    # run_examples_auth()
+    # run_user_example(client)
+    # find_folder_examples(client)
+    # run_collab_examples(client)
+    # # Premium Apps only
+    # upload_accelerator(client)
+
+
 
 if __name__ == '__main__':
     main()
+
